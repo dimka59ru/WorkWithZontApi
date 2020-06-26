3bc18fe63767f8da6a1f5fc199806462cefb64c5
Dmitriy <dimka59ru@gmail.com>
2020-06-26 20:19:37 +0500

    Add Zont Service

--- WorkWithZontApi/Pages/Index.cshtml.cs
+++ WorkWithZontApi/Pages/Index.cshtml.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using WorkWithZontApi.Services;
 
 namespace WorkWithZontApi.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ZontService _zontService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ZontService zontService)
         {
             _logger = logger;
+            _zontService = zontService;
         }
 
         public void OnGet()
